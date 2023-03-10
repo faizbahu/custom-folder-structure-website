38bb695ec09d7156be10a163ba3c82f62ba24e9e
JawadR1 <jawadrasheed97@gmail.com>
2023-03-10 14:17:18 +0500

    Added Layout File

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Dashboard
+            </Link>
+
+            <template v-else>
+                <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in
+                </Link>
+
+                <Link :href="route('register')" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Register
+                </Link>
+            </template>
+        </div>
+
+        <slot/>
+    </div>
+</template>
